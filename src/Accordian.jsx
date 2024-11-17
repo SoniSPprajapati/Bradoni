9810e54770ba2f25d89c2aed262cad568480ca9b
SoniSPprajapati <prajapatisoni839@gmail.com>
2024-11-17 09:19:59 +0000

    final code

---
+++ src/Accordian.jsx
@@ -0,0 +1,27 @@
+import Accordion from "./Accordion";
+
+function Bellow({
+  data,
+  showUnderline = false,
+  isOpen = false,
+  onClick = () => {},
+}) {
+  return (
+    <div>
+      {data.map((obj) => {
+        return (
+          <Accordion
+            key={obj.id}
+            title={obj.title}
+            description={obj.description}
+            showUnderline={showUnderline}
+            isOpen={isOpen}
+            onClick={() => onClick(obj.title)}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default Bellow;
---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import configs from "./assets/data.json";
+import Bellow from "./Bellow.jsx";
+
+function App() {
+  return (
+    <div className="flex flex-col gap-1 items-center justify-center min-h-screen">
+      <h1 className="text-xl font-bold">Last Selected: None</h1>
+      <Bellow
+        data={configs}
+        showUnderline={true}
+        isOpen={false}
+        onClick={(value) => {
+          alert(value);
+        }}
+      />
+    </div>
+  );
+}
+
+export default App;
